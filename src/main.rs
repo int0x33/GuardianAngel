7e75b411cb557547aa71c698f224490757b6d235
ZER0 <46683415+int0x33@users.noreply.github.com>
2019-02-14 15:28:16 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 extern crate reqwest;
-use std::io::Read;
 use std::process::Command;
+
+fn passed_check()
+{
+    println!("OK");
+}
+
+fn failed_check()
+{
+    let mut list_dir = Command::new("ls");
+    list_dir.status().expect("process failed to execute");
+}
 
 fn heartbeat()
 {
-    let mut response = reqwest::get("https://httpbin.org/status/500").expect("Failed to send request");
+    let response = reqwest::get("https://httpbin.org/status/200").expect("Failed to send request");
     if response.status() == 200 {
-        println!("{}", response.status());
-        let mut buf = String::new();
-        response.read_to_string(&mut buf).expect("Failed to read response");
-        println!("{}", buf);
+        passed_check();
     } else {
-        let mut list_dir = Command::new("ls");
-        list_dir.status().expect("process failed to execute");
+        failed_check();
     }
 }
 
